3d68391067653d69e7f204aad5f071a8b386fcff
rj7rivera <145302115+rj7rivera@users.noreply.github.com>
2024-02-18 12:37:39 -0500

    Add files via upload

---
+++ app_con_js/app_con_js/js/script.js
@@ -0,0 +1,44 @@
+        // Obtén las referencias a los elementos
+        const monedaEl_one = document.getElementById('moneda-uno');
+        const monedaEl_two = document.getElementById('moneda-dos');
+        const cantidadEl_one = document.getElementById('cantidad-uno');
+        const cantidadEl_two = document.getElementById('cantidad-dos');
+        const cambioEl = document.getElementById('cambio');
+        const tazaEl = document.getElementById('taza');
+        const realizarConversionBtn = document.getElementById('realizarConversion');
+
+        // Fetch Exchange Rate and Update the DOM
+        function calculate() {
+            const moneda_one = monedaEl_one.value;
+            const moneda_two = monedaEl_two.value;
+
+            fetch(`https://api.exchangerate-api.com/v4/latest/${moneda_one}`)
+                .then(res => res.json())
+                .then(data => {
+                    const taza = data.rates[moneda_two];
+
+                    cambioEl.innerText = `1 ${moneda_one} = ${taza} ${moneda_two}`;
+
+                    cantidadEl_two.value = (cantidadEl_one.value * taza).toFixed(2);
+
+                });
+        }
+
+        // Event listeners
+        monedaEl_one.addEventListener('change', calculate);
+        cantidadEl_one.addEventListener('input', calculate);
+        monedaEl_two.addEventListener('change', calculate);
+        cantidadEl_two.addEventListener('input', calculate);
+
+        tazaEl.addEventListener('click', () => {
+            const temp = monedaEl_one.value;
+            monedaEl_one.value = monedaEl_two.value;
+            monedaEl_two.value = temp;
+            calculate();
+        });
+
+        // Agregar el evento clic al botón "Realizar Conversión"
+        realizarConversionBtn.addEventListener('click', calculate);
+
+        // Llamar a la función calculate al cargar la página
+        calculate();
